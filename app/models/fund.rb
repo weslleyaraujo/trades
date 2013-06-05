2931285a10dccd5f5c8f8cae5d5881fe35555e37
Fabiano Beselga <fabianobeselga@gmail.com>
2013-06-05 10:35:07 -0300

    created funds model

---
+++ app/models/fund.rb
@@ -0,0 +1,3 @@
+class Fund < ActiveRecord::Base
+  attr_accessible :name
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 FrontEndTest::Application.routes.draw do
+  resources :funds
+
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
