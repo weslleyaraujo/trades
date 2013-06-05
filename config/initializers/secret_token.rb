7066868e15bdc9cc930b57133a3e7a0b764a8afa
Fabiano Beselga <fabianobeselga@gmail.com>
2013-06-05 08:04:40 -0300

    rails bootstrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FrontEndTest::Application.config.secret_token = '3621b7d33e947acff0446f4e9184bf94b38f9d02af9b1dee114202d28cbea93e401cbb05251fcfe11251184a4fb4fa50b4acbbc54908c55c6e9f7b140cca930e'
