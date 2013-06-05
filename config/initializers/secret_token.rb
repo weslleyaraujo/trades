7bd1f33df0ba460cefc11328c1e76084ef5b29d3
Fabiano Beselga <fabianobeselga@gmail.com>
2013-06-05 09:32:23 -0300

    usql sqlite

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-FrontEndTest::Application.config.secret_token = '3621b7d33e947acff0446f4e9184bf94b38f9d02af9b1dee114202d28cbea93e401cbb05251fcfe11251184a4fb4fa50b4acbbc54908c55c6e9f7b140cca930e'
+FrontEndTest::Application.config.secret_token = 'ccef6524e4beeb812e141498ca303e761a1705005d3474cf7664e9db775c43eed09a1eb6a3fc241206d0354812e2daf8ca6f16c2cc59c738cef86d7e5b972d07'
