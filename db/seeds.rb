8ca85cd7a76dabd0d5c4fd475f6dfbc62ebba942
Fabiano Beselga <fabianobeselga@gmail.com>
2013-06-07 10:30:22 -0300

    created db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+#
+
+
+Fund.delete_all
+Price.delete_all
+Trade.delete_all
+
+fund = Fund.create(:name => 'Sul America Expertise Fundo de Investimento em Ações')
+
+last_price = rand(20) + 1
+(1.year.ago.to_date..Date.yesterday).each do |date|
+  daily_return = rand(800) / 10_000.to_f
+  price = (last_price * (1 + daily_return)).round(8)
+  last_price = price
+  puts price
+  fund.prices.create(:date => date, :price => price)
+end
