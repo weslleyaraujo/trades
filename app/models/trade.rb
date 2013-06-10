656a90b44c3f3ddfba29092acaa669ad2782e9b6
Fabiano Beselga <fabianobeselga@gmail.com>
2013-06-10 11:14:06 -0300

    change kind validation

--- app/models/trade.rb
+++ app/models/trade.rb
@@ -1,4 +1,7 @@
 class Trade < ActiveRecord::Base
   belongs_to :fund
   attr_accessible :date, :kind, :shares
+
+  validates :kind, :inclusion => { :in => [0, 1],
+        :message => "%{value} is not a valid, it should be 0 for 'subscription' or 1 for 'redemption'" }
 end
