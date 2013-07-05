c366614c700838595422f8b35baec960be708d7a
Luciano Tavares <lucianomt@gmail.com>
2013-07-05 00:20:20 -0300

    added events, change selector to data-js

--- app/assets/javascripts/trades.js
+++ app/assets/javascripts/trades.js
@@ -1,15 +1,20 @@
 Module("FrontEnd.TradesController.Index", function(Index) {
   Index.fn.initialize = function(container) {
     this.container = container;
-    this.button = $(container).find('.add-trade-button');
+    this.button = $(container).find('[data-js=add-trade-button]');
 
     this.addEventListeners();
   };
 
   Index.fn.addEventListeners = function() {
     this.button
+      .on('mouseover', this.onButtonMouseOver.bind(this))
       .on('click', this.onButtonClick.bind(this))
     ;
+  };
+
+  Index.fn.onButtonMouseOver = function(event) {
+    console.log('hova');
   };
 
   Index.fn.onButtonClick = function(event) {
