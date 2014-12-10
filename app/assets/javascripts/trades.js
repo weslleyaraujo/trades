2da9c8aa46aedbc39aefa76b88485183b57c720b
Weslley Araujo <weslleyaraujo20@gmail.com>
2014-12-10 00:39:40 -0200

    Adding buttons to view

--- app/assets/javascripts/trades.js
+++ app/assets/javascripts/trades.js
@@ -1,2 +1,26 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+/*
+ * Main file for global configs
+ *
+ * This file will be the first import after vendor,
+ * all config values must be here
+ * */
+window.Trades = {};
+
+Trades.Backbone = {
+  models: {},
+  views: {},
+  collections: {}
+};
+
+Trades.helpers = {};
+
+/*
+ * Helper for underscore template
+ *
+ * @method template
+ * @return {Function} Returns the template function
+ * */
+Trades.helpers.template = function (selector) {
+  var content = $(selector).html();
+  return _.template(content ? content : "");
+};
